9656b76e56634055076c45b63dee30819b3aa60c
qazwsx92284 <qazwsx92284@gmail.com>
2026-05-27 00:31:53 -0400

    refactor the promptTemplate by adding .st file in resources

--- src/main/java/com/min0/openai/controller/PromptTemplateController.java
+++ src/main/java/com/min0/openai/controller/PromptTemplateController.java
@@ -1,6 +1,8 @@
 package com.min0.openai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,16 +18,9 @@ public class PromptTemplateController {
         this.chatClient = chatClient;
     }
 
-    String promptTemplate =
-            """
-             A customer named {customerName} sent the following message:
-             "{customerMessage}"
-             
-             Write a polite and helpful email response addressing the issue.
-             Maintain a professional tone and provide reaaurance.
-             
-             Respond as if you're writing the email body only. Don't include subject and signature.
-            """;
+    @Value("classpath:promptTemplate/userPromptTemplate.st")
+    Resource userPromptTemplate;
+
 
     // Suppose customer support team to get the potential email responses that
     // they can send to the customer based upon the complaint they raise.
@@ -38,7 +33,7 @@ public class PromptTemplateController {
                              you are a professional customer service assistant which helps drafting email responses to improve the \s
                              productivity of the customer support team.
                             """)
-                .user(promptTemplateSpec -> promptTemplateSpec.text(promptTemplate)
+                .user(promptTemplateSpec -> promptTemplateSpec.text(userPromptTemplate)
                         .param("customerName", custName)
                         .param("customerMessage", custMsg))
                 .call().content();
