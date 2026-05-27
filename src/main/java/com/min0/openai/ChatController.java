0d87e9b3f0abfa8391d3b54dc15b483dc67069c8
qazwsx92284 <qazwsx92284@gmail.com>
2026-05-26 22:21:09 -0400

    defaultSystem() and defaultUser() example

--- src/main/java/com/min0/openai/ChatController.java
+++ src/main/java/com/min0/openai/ChatController.java
@@ -14,7 +14,16 @@ public class ChatController {
     private final ChatClient chatClient;
 
     public ChatController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+        this.chatClient = chatClientBuilder
+                // let's say I have 10 endpoints that has the same system role, I can use defaultSystem() method to remove redundancy
+                .defaultSystem("""
+                        You are an internal HR assistant. Your role is to help employees with questions related to HR policies,\s
+                        such as leave policies, working hours, benefits, and code of conduct.
+                        If a user asks for help with anything outside of these topics, kindly inform them that you can only assist\s
+                        with queries related to HR policies.
+                        """)
+                .defaultUser("How can you help me?")
+                .build();
     }
 
     @GetMapping("/chat")
@@ -25,11 +34,20 @@ public class ChatController {
          */
         return chatClient
                 .prompt() // we can send prompt() with empty param and then specify type of message following like system/user/assistant/function
+                //.user(message) if I don't pass .user(), the defaultUser() above with the default msg will be sent.
+                .call().content();
+    }
+
+    @GetMapping("/IT/chat")
+    public String ITchat(@RequestParam("message") String message) {
+        return chatClient
+                .prompt()
+                // we can override the default system role like below
                 .system("""
-                        You are an internal HR assistant. Your role is to help employees with questions related to HR policies,\s
-                        such as leave policies, working hours, benefits, and code of conduct.
-                        If a user asks for help with anything outside of these topics, kindly inform them that you can only assist\s
-                        with queries related to HR policies.
+                        Yuo are an internal IT helpdesk assistant. Your role is to assist employees with IT-related issues such as \s
+                        resetting password, unlocking accounts, and answering questions related to IT policies.
+                        If a user requests help with anything outside of these responsibilities, respond politely and inform them that \s
+                        you are only able to assist with IT support tasks within your defined scope.
                         """)
                 .user(message)
                 .call().content();
